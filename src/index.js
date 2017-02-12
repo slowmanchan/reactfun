de594d8b95bf93b46a45d6d896b533e050b76260
Norman Chan <slowmanchan@gmail.com>
2017-02-12 16:10:51 -0500

    new code block

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+require('./main.css');
+
 const MLB = require('./mlb.json');
 
 class StatRow extends Component {
@@ -56,9 +58,9 @@ class BatterRow extends Component {
       rows.push(<tr><td>{row.name_display_first_last}</td><td>{row.ab}</td><td>{row.r}</td><td>{row.h}</td><td>{row.rbi}</td><td>{row.bb}</td><td>{row.so}</td><td>{row.avg}</td></tr>)
     })
     return (
-      <div>
+      <tr>
         {rows}
-      </div>
+      </tr>
     )
   }
 }
@@ -157,7 +159,7 @@ class DetailsButton extends Component {
 
   clickHandler() {
     this.setState({
-      showComponent: true,
+      showComponent: true
     })
   }
 
@@ -172,38 +174,6 @@ class DetailsButton extends Component {
   }
 }
 
-// class SearchBar extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       date: ''
-//     }
-//     this.handleChange = this.handleChange.bind(this);
-//     this.handleSubmit = this.handleSubmit.bind(this);
-//   }
-//
-//   handleChange(e) {
-//     this.setState({
-//       date: e.target.value
-//     })
-//   }
-//
-//   handleSubmit(e) {
-//     e.preventDefault();
-//     console.log(this.state.date)
-//   }
-//
-//   render() {
-//     return (
-//       <form style={{textAlign: 'center'}} onSubmit={this.handleSubmit}>
-//         <input type="date" placeholder="Enter a date..." onChange={this.handleChange}>
-//         </input>
-//         <input type="submit"></input>
-//       </form>
-//     )
-//   }
-// }
-
 class DateSelector extends Component {
   render() {
     return (
@@ -217,14 +187,25 @@ class DateSelector extends Component {
 }
 
 class Scores extends Component {
-  render() {
-    const {home_code, linescore, away_code, status} = this.props.data;
 
+  render() {
+    let {home_code, linescore, away_code, status} = this.props.data;
+
+
+    let homeWinning = false;
+    let awayWinning = false;
+    // check to see which is the winning scores
+    if (status.status !== 'Preview') {
+      homeWinning = linescore.r.home > linescore.r.away;
+      awayWinning = linescore.r.away > linescore.r.home;
+    }
+
+    // display scores and bolding the winning team
     return (
       <div className="scores" style={{display: 'inline-block', margin: '20px'}}>
         <h4>{status.status}</h4>
-          <p>{home_code} | {linescore.r.home}</p>
-          <p>{away_code} | {linescore.r.away}</p>
+          <p className={homeWinning ? 'bold' : null}>{home_code} | {linescore.r.home}</p>
+          <p className={awayWinning ? 'bold' : null}>{away_code} | {linescore.r.away}</p>
         <DetailsButton />
       </div>
     );
@@ -237,56 +218,134 @@ class ScoreBoard extends Component {
 
     this.state = {
       games: [],
-      date: ''
+      date: '',
+      noGameData: true,
+      details: null,
+
     }
     this.fetchData = this.fetchData.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
+  // fetch data from MLB JSON API
   fetchData(month, day, year) {
     axios.get(`http://gd2.mlb.com/components/game/mlb/year_${year}/month_${month}/day_${day}/master_scoreboard.json`)
       .then((res) => {
+
         console.log(res.data.data.games.game)
 
-        this.setState({
-          games: res.data.data.games.game
-        })
+        // 1 or more games, obj or array.
+        if (res.data.data.games.game) {
+
+          let games;
+          // Just one game: single object
+          if (typeof res.data.data.games.game.length === "undefined") {
+            games = [res.data.data.games.game]
+          } else {
+            games = res.data.data.games.game;
+          }
+
+          // map over games, create copy of games and if game is missing linescore, create
+          // blank linescore
+
+          games = games.map((game, idx) => {
+            let gameCopy = Object.assign({}, game);
+
+            if (!gameCopy.hasOwnProperty('linescore')) {
+              gameCopy.linescore = {
+                r: {
+                  home: '',
+                  away: '',
+                }
+              };
+            }
+            return gameCopy;
+          })
+
+          let gamesToronto = [];
+          let gamesNotToronto = [];
+          games.forEach((game) => {
+
+            // Store toronto game.
+            if (game.away_code === "tor" || game.home_code === "tor") {
+              gamesToronto.push(game);
+            } else {
+              gamesNotToronto.push(game); //Not toronto game.
+            }
+          })
+
+          gamesToronto = gamesToronto.concat(gamesNotToronto)
+
+
+          this.setState({
+            games: gamesToronto,
+            noGameData: false,
+          });
+        } else {
+          this.setState({
+            games: [],
+            noGameData: true,
+          })
+        }
       })
+
       .catch((error) => {
-        console.log(error.response)
+        this.setState({
+          games: [],
+          noGameData: true,
+        })
       });
   }
-
+  /*
   componentDidMount() {
     // add function to get last game played
 
     this.fetchData(10, 11, 2016);
   }
-
+  */
   handleChange(e) {
+    /*
     this.setState({
       date: e.target.value
     });
-    console.log(this.state.date)
+    */
+    const dateString = e.target.value.split('-');
+    const year = dateString[0];
+    const month = dateString[1];
+    const day = dateString[2];
+
+    this.fetchData(month, day, year)
   }
 
 
 
   render() {
+
+    const checkGameData = () => {
+      if (this.state.noGameData) {
+        return (
+          <div>
+            No games found
+          </div>
+        );
+      }
+    }
+
     var scores = [];
 
     this.state.games.forEach((game, index) => {
       scores.push(<Scores data={game} key={index} />);
     });
-    console.log(this.state.games);
+
     return (
       <div>
       <form style={{textAlign: 'center'}} onSubmit={this.handleSubmit}>
         <input type="date" placeholder="Enter a date..." onChange={this.handleChange} />
-        <input type="submit"></input>
       </form>
       <DateSelector date={this.state.games.modified_date} />
+        {checkGameData()}
         {scores}
+        <DetailsBox />
       </div>
     );
   }
